674faa645435b3caef3b1bfd255663dd4ed3651a
Skybax <skybax14@gmail.com>
2019-12-17 14:24:51 -0800

    Tuple data structure

---
+++ Portfolio/tuples.py
@@ -0,0 +1,17 @@
+# List: []
+# Dictionary: {}
+# Tuple: ()
+
+post = ('Python Basics', 'Intrro guide to python', 'Some cool python content')
+
+# Tuple unpacking
+title, sub_heading, content = post
+
+# Equivalent to Tuple unpacking
+# title = post[0]
+# sub_heading = post[1]
+# content = post[2]
+
+print(title)
+print(sub_heading)
+print(content)
