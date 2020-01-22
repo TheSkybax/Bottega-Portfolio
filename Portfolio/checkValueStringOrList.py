90216ad9ef3d60b658eaba8a97980a3d92e9ef0a
Skybax <skybax14@gmail.com>
2020-01-21 16:01:25 -0800

    How to Check if a Value is Included in a Python String or List

---
+++ Portfolio/checkValueStringOrList.py
@@ -0,0 +1,14 @@
+sentence = 'The quick brown fox jumped over the lazy dog'
+word = 'Dog'
+
+if word in sentence:
+    print('The word was found in the sentence')
+else:
+    print('The word was not in the sentence')
+
+nums = [1, 2, 3, 4]
+
+if 3 in nums:
+    print('Number was found')
+else:
+    print('Number was not found')
