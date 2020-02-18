e07998bcbff1dcf6219c4259ace1a49c430f7011
Skybax <skybax14@gmail.com>
2020-02-18 12:55:04 -0800

    How to Create a Custom Module and Import It In the Python Repl

---
+++ Portfolio/customeModuleImport.py
@@ -0,0 +1,12 @@
+import math
+
+print(math.sqrt(25))
+
+# Place in file helper.py
+def greeting(first, last):
+  return f'Hi {first} {last}!'
+
+# Call from repl
+import helper
+
+helper.greeting('Kristine', 'Hudgens')
