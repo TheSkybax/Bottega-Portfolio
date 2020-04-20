33f7040bdae722dff1dc78ab3da085c3b8c9ef7a
Skybax <skybax14@gmail.com>
2020-04-20 14:00:17 -0700

    How to Swap Variable Values in JavaScript with Variable Deconstruction

---
+++ Portfolio/JS/variableDeconstruction.js
@@ -0,0 +1,9 @@
+let playerOne = 'Tiffany';
+let playerTwo = 'Kristine';
+
+[playerOne, playerTwo] = [playerTwo, playerOne];
+
+console.log(`
+  Player One: ${playerOne}
+  Player Two: ${playerTwo}
+  `);
