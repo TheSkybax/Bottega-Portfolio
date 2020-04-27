016aa8568c01dd31f6d51901b523f04f080543f2
Skybax <skybax14@gmail.com>
2020-04-26 22:17:32 -0700

    Guide to JavaScript OOP Instance Methods

---
+++ Portfolio/JS/instanceMethodsOOP.js
@@ -0,0 +1,15 @@
+class Instructor {
+    constructor({ name, role= 'assistant' }) {
+        this.name = name;
+        this.role = role;
+    }
+
+    renderDetails() {
+        console.log(`${this.name}: ${this.role}`);
+    }
+}
+
+const jon = new Instructor({name: 'Jon Snow'});
+const brayden = new Instructor({name: 'Brayden', role: 'teacher'});
+jon.renderDetails();
+brayden.renderDetails();
