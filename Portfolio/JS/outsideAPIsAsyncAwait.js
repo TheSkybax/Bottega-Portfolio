f00728f37dc85f5098124a2a1f3cdf79c8fb8403
Skybax <skybax14@gmail.com>
2020-05-05 14:22:44 -0700

    Implementing Error Handling In a JavaScript Async / Await Function

--- Portfolio/JS/outsideAPIsAsyncAwait.js
+++ Portfolio/JS/outsideAPIsAsyncAwait.js
@@ -2,13 +2,23 @@
 // https://api.dailysmarty.com/posts
 
 async function queryApis() {
-    const postsPromise = fetch('https://api.dailysmarty.com/posts');
+  try {
+    const postsPromise = fetch('http://api.dailysmarty.com/posts');
     const posts = await postsPromise.then(res => res.json());
     console.log(posts);
-  
+  } catch(err) {
+    console.log(err);
+    console.log('There was an error with the DailySmarty API');
+  }
+
+  try {
     const reposPromise = fetch('https://api.github.com/users/jordanhudgens/repos');
     const repos = await reposPromise.then(res => res.json());
     console.log(repos);
+  } catch(err) {
+    console.log(err);
+    console.log('There was an error with the GitHub API');
   }
-  
-  queryApis();
+}
+
+queryApis();
