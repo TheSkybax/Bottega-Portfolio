bf4d87f1706641d0066b04dc642813e18c2ad1f8
Skybax <skybax14@gmail.com>
2020-06-07 13:36:44 -0700

    Build a Manual Exponent Function in JavaScript

---
+++ Portfolio/JS/functionManualExponent.js
@@ -0,0 +1,9 @@
+const toThePowerOf = (num, exp) => {
+    const items = Array(exp).fill(num);
+    const reducer = (total, currentValue) => total * currentValue;
+    return items.reduce(reducer);
+}
+
+toThePowerOf(2, 3);//?
+toThePowerOf(3, 3);//?
+toThePowerOf(10, 10);//?
