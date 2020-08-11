4fad4f6cdb9f5e0ef5e4500926f02dea7a1b20e5
Skybax <skybax14@gmail.com>
2020-08-11 16:40:59 -0700

    A function that capitalizes every word

--- Portfolio/JS/toCapital.js
+++ Portfolio/JS/toCapital.js
@@ -3,7 +3,6 @@ const toCapital = str => {
     return words.map(word => word[0].toUpperCase() + word.slice(1)).join(" ");
 }
 
-const shortStr = "Hi there";
 const longStr = "the quick brown fox jumped over the lazy dog";
 
 toCapital(longStr); //?
